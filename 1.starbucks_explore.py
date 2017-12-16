081ab7806e240834a44f86a528fbe88900d8f655
Alex Pan <Alexanderypan@gmail.com>
2017-12-15 21:26:21 -0500

    Expanded on exploratory analyses and Basemap

--- 1.starbucks_explore.py
+++ 1.starbucks_explore.py
@@ -19,35 +19,103 @@ data_starbucks.head()
 data_starbucks.isnull().sum()    # Data are pretty clean for the most part.
 
 # Number of stores per country?
-data_starbucks['Country'].value_counts()    # Top 3: US, China, Canada
+data_starbucks['Country'].value_counts()[:25]    # Top 3: US, China, Canada
 
-data_starbucks['Country'].value_counts().plot('bar')
+data_starbucks['Country'].value_counts()[:25].plot('bar')
+plt.show()
+
+
+# Which cities have the most Starbucks?
+
+data_starbucks['City'].value_counts()[:25]
+
+# Toronto has a lot of Starbucks.
+# 上海市 (#1) is Shanghai, 北京市 (#3) is Beijing
+# 서울 (#14) is Seoul, which actually appears as 'Seoul' in position #2
+# 杭州市 (#22) is Hangzhou, 深圳市 (#23) is Shenzhen, 广州市 (#25) is Guangzhou.
+
+# Some of these cities have English names listed here as well.
+# Change these names to English:
+city_names = {'上海市': 'Shanghai',
+              '北京市': 'Beijing',
+              '서울': 'Seoul',
+              '杭州市': 'Hangzhou',
+             '深圳市': 'Shenzhen',
+             '广州市': 'Guangzhou'}
+
+data_starbucks = data_starbucks.replace({'City': city_names})
+cities_starbucks = data_starbucks['City'].value_counts()
+
+cities_starbucks[:25].plot('bar', width = 0.9)
+plt.title('Cities with the most Starbucks Locations')
+plt.ylabel('Number of Starbucks stores')
+plt.tight_layout()
+plt.show()
+
+
+# Which city has the highest Starbucks to people ratio?
+
+# Load in Population data:
+# Data from https://nordpil.com/resources/world-database-of-large-cities/
+# Which I believe uses data from the UN
+data_populations = pd.read_csv("World City Populations.csv",
+                               encoding='latin1')
+
+# *** DISCLAIMER: The UN data aggregate municipalities into large urban areas,
+# whereas the Starbucks data list their store locations by municipality (i.e.,
+# the number of Starbucks locations per urban area are underestimated). Example:
+# Mississauga and Hamilton fall under the 'Toronto' urban area in the UN data,
+# but are listed separately in the Starbucks data. ***
+
+
+# Data are a bit dirty, so some clean up:
+mask_nulls = data_populations['City'].isnull()
+data_populations.loc[mask_nulls, 'City'] = data_populations.loc[mask_nulls, 'ID']
+
+data_populations = data_populations.set_index('City')
+###################
+
+# Number of stores / population of the city
+density_starbucks = cities_starbucks / \
+                    data_populations.loc[cities_starbucks.keys()]['pop2015']
+
+# Some cities not included in population data, or have incompatible characters
+density_starbucks = density_starbucks[density_starbucks.notnull()]
+
+# Population data are in 1000s, multiply by 100 to express as stores per 100,000
+density_starbucks = density_starbucks * 100
+
+density_starbucks.sort_values(ascending = False)[:25].plot('bar', width=0.9)
+plt.title('Hippest Places on Earth: Cities with the highest density ' +
+          'of Starbucks Stores (2015 population)')
+plt.ylabel('Number of Starbucks stores per 100,000 people')
+plt.tight_layout()
 plt.show()
 
 
 #############
 
+# #
+# # Now looking at Canadian Stores Only.
+# #
 #
-# Now looking at Canadian Stores Only.
+# data_canada = data_starbucks[data_starbucks['Country'] =='CA']
 #
-
-data_canada = data_starbucks[data_starbucks['Country'] =='CA']
-
-
-data_canada.head()
-
-
-# Starbucks Locations by Province
-data_canada['State/Province'].value_counts()
-data_canada['State/Province'].value_counts().plot('bar')
-plt.show()
-
-
-
-
-data_canada['Latitude'].max()
-data_canada['Latitude'].min()
-data_canada['Latitude'].median()
-# Our Starbucks stores call between 42.25 and 60.7 longitude. Median = 49.
+#
+# data_canada.head()
+#
+#
+# # Starbucks Locations by Province
+# data_canada['State/Province'].value_counts()
+# data_canada['State/Province'].value_counts().plot('bar')
+# plt.show()
+#
+#
+#
+#
+# data_canada['Latitude'].max()
+# data_canada['Latitude'].min()
+# data_canada['Latitude'].median()
+# # Our Starbucks stores call between 42.25 and 60.7 longitude. Median = 49.
 
 
--- 2.starbucks_distances.py
+++ 2.starbucks_distances.py
@@ -1,23 +1,25 @@
+
 import numpy as np
 import pandas as pd
 import math
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 
+map_resolution='c'    # 'c' for development, 'f' to publish
 data_starbucks = pd.read_csv('Starbucks Locations.csv')
 
 #
 # The aim of this script is to be able to work out distances between each
 # Starbucks location based on their GPS coordinates.
 #
-# Subtraction of two coordinates should provide their Euclidean distances,
+# Subtraction of two coordinates should provide their Great Circle distances,
 # assuming that the Earth is spherical.
 #
 
 ###
 
 class Coordinate(object):
-	def __init__(self, lat, lon):
+	def __init__(self, lon, lat):
 		self.lat = lat
 		self.lon = lon
 
@@ -57,16 +59,68 @@ data_starbucks['GPS'] = \
                          axis = 'columns'
                          )
 
+
+# Examples of stupid stuff you can do:
+
+# How many stores are within __km of my house?
+km = 10
+my_house = Coordinate(-79.40, 43.66)
+
+data_starbucks['dist'] = data_starbucks.apply(
+	lambda row: my_house - row['GPS'],
+	axis = 'columns')
+
+print(data_starbucks[data_starbucks['dist'] <= km])
+
+# Which store is nearest to my house?
+closest = data_starbucks.sort_values('dist').head(1)
+
+#           Brand Store Number                      Store Name Ownership Type  \
+# 1719  Starbucks   72457-7627  U of Toronto - Robarts Library       Licensed
+#      Street Address     City State/Province Country Postcode  Phone Number  \
+# 1719  369 Huron St.  Toronto             ON      CA  M5S 1A5  416-591-1647
+#                        Timezone  Longitude  Latitude             GPS      dist
+# 1719  GMT-05:00 America/Toronto      -79.4     43.66  (43.66, -79.4)  0.540616
+
+# Which store is the furthest away from my house?
+furthest = data_starbucks.sort_values('dist', ascending=False).head(1)
+
+#          Brand  Store Number          Store Name Ownership Type  \
+# 290  Starbucks  25286-240412  DFO Spencer Street       Licensed
+#                            Street Address       City State/Province Country  \
+# 290  93-161 Spencer Street, 201, 202, 203  Melbourne            VIC      AU
+#     Postcode Phone Number                       Timezone  Longitude  Latitude  \
+# 290     3000          NaN  GMT+10:00 Australia/Melbourne     144.95    -37.82
+#                   GPS         dist
+# 290  (-37.82, 144.95)  16268.21892
+
+# I will have to visit.
+
 #
-# World Map plots
+#
+#
+#
 #
 
-m = Basemap(projection='merc',llcrnrlat=-60,urcrnrlat=80,\
-            llcrnrlon=-180,urcrnrlon=180,lat_ts=20,resolution='l')
-m.drawcoastlines()
-m.fillcontinents(color='white',lake_color='deepskyblue')
-# draw parallels and meridians.
+# World Map with Starbucks Locations
+plt.clf()
+m = Basemap(projection='merc',llcrnrlat=-60,urcrnrlat=84,\
+            llcrnrlon=-180,urcrnrlon=180,resolution=map_resolution)
 m.drawmapboundary(fill_color='deepskyblue')
-# m.bluemarble()    # NASA Background
-plt.title("Mercator Projection")
+m.fillcontinents(color='white',lake_color='deepskyblue')
+# m.drawmeridians(np.arange(-180,180,30),labels=[1,1,0,1])
+# m.drawparallels(np.arange(10,90,20),labels=[1,1,0,1]
+lat, lon = m(np.array(data_starbucks['Longitude']),
+             np.array(data_starbucks['Latitude']))
+m.plot(lat, lon, 'o', color='k', markersize='1', alpha = 0.25)
+plt.title("Starbucks Locations Around The World")
 
+# m.drawcoastlines(color='white')
+
+# Some more useless stuff you can do:
+
+# Draw a great circle line fom my nearest Starbucks location to my furthest:
+lon1, lat1 = float(closest['Longitude']), float(closest['Latitude'])
+lon2, lat2 = float(furthest['Longitude']), float(furthest['Latitude'])
+
+m.drawgreatcircle(lon1, lat1, lon2, lat2, alpha = 0.5)
